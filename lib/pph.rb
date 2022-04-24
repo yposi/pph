62e60a57ddbb7e1af422ef04a25946dcec86faf5
Shuhei Yamashita <y.shuhei0501@gmail.com>
2022-04-24 21:14:55 +0900

    first commit

---
+++ lib/pph.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pph/version"
+
+module Pph
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pph/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Pph
+  VERSION = "0.1.0"
+end
